6f02283d4ede5b15e7a322d14db4b347be0f3a70
Landon Taylor <landon.jeffrey.taylor@usu.edu>
2024-04-29 09:16:05 -0600

    fix indentation

--- yeast_polarization.py
+++ yeast_polarization.py
@@ -4,6 +4,7 @@ int_t = Types.int_type()
 bool_t = Types.bool_type()
 zero = Terms.rational(0, 1)
 one = Terms.rational(1, 1)
+
 class Unroller(object):
     def __init__(self, state_vars, nexts, inputs):
         self.state_vars = state_vars
@@ -21,22 +22,23 @@ class Unroller(object):
                                                Terms.to_string(v) + "@" + str(k))
             self.var_cache[(v, k)] = v_k
         return self.var_cache[(v, k)]
-        def _get_cache_at_time(self, k):
-            assert(k >= 0)
-            while len(self.time_cache) <= k:
-                cache = dict()
-                t = len(self.time_cache)
-                for s in self.state_vars:
-                    s_t = self.get_var(s, t)
-                    n_t = self.get_var(s, t+1)
-                    cache[s] = s_t
-                    cache[self.nexts[s]] = n_t
-                    for i in self.inputs:
-                        i_t = Terms.new_uninterpreted_term(Terms.type_of_term(i),
-                                                               Terms.to_string(i) + "@" + str(t))
-                        cache[i] = i_t
-                        self.time_cache.append(cache)
+    def _get_cache_at_time(self, k):
+        assert(k >= 0)
+        while len(self.time_cache) <= k:
+            cache = dict()
+            t = len(self.time_cache)
+            for s in self.state_vars:
+                s_t = self.get_var(s, t)
+                n_t = self.get_var(s, t+1)
+                cache[s] = s_t
+                cache[self.nexts[s]] = n_t
+            for i in self.inputs:
+                i_t = Terms.new_uninterpreted_term(Terms.type_of_term(i),
+                                                Terms.to_string(i) + "@" + str(t))
+                cache[i] = i_t
+            self.time_cache.append(cache)
         return self.time_cache[k]
+
 # state variables
 R = Terms.new_uninterpreted_term(int_t, 'R')
 L = Terms.new_uninterpreted_term(int_t, 'L')
@@ -78,15 +80,13 @@ INIT = Terms.yand([Terms.eq(R, Terms.rational(50, 1)),
                    Terms.eq(GBG, Terms.rational(0, 1)),
                    Terms.eq(GD, Terms.rational(0, 1))
                    ])
-# R1 = Terms.yand([Terms.implies(c1,
-#                     Terms.yand([Terms.eq(nexts[R], Terms.add(R, one)),
-#                                     frame_cond([L, RL, G, GA, GBG, GD])])),
-#                  Terms.implies(Terms.ynot(c1),
-#                                frame_cond([R, L, RL, G, GA, GBG, GD]))])
-R2 = Terms.implies(Terms.arith_geq_atom(R, one), Terms.sub(nexts[R], one))
-print("type of sub is ", Terms.type_of_term(Terms.sub(nexts[R], one)))
-print("type of arith_geq_atom is ", Terms.type_of_term(Terms.arith_geq_atom(R, one)))
-R3 = Terms.implies(Terms.yand([Terms.arith_geq_atom(L, one), Terms.arithm_geq_atom(R, one)]),
+R1 = Terms.yand([Terms.implies(c1,
+                    Terms.yand([Terms.eq(nexts[R], Terms.add(R, one)),
+                                    frame_cond([L, RL, G, GA, GBG, GD])])),
+                 Terms.implies(Terms.ynot(c1),
+                               frame_cond([R, L, RL, G, GA, GBG, GD]))])
+R2 = Terms.implies(Terms.arith_geq(R, one), Terms.sub(nexts[R], one))
+R3 = Terms.implies(Terms.yand([Terms.arith_geq(L, one), Terms.arithm_geq(R, one)]),
                    Terms.yand([Terms.eq(nexts[L], L),
                                Terms.eq(nexts[R], Terms.sub(R, one)),
                                Terms.eq(nexts[RL], Terms.add(RL, one)),
@@ -95,7 +95,7 @@ R3 = Terms.implies(Terms.yand([Terms.arith_geq_atom(L, one), Terms.arithm_geq_at
                                Terms.eq(nexts[GBG], GBG),
                                Terms.eq(nexts[GD], GD)]))
 TRANS = Terms.yand([R1, R2, R3])
-GOAL = Terms.arith_q_atom(GBG, Terms.rational(50, 1))
+GOAL = Terms.arith_geq_atom(GBG, Terms.rational(50, 1))
 print("INIT := " + Terms.to_string(INIT))
 print("TRANS := " + Terms.to_string(TRANS))
 print("GOAL := " + Terms.to_string(GOAL))
